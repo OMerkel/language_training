43143f93b343dc439a65509b91e5b6860af0da21
Oliver Merkel <merkel.oliver@web.de>
2025-05-19 21:40:09 +0200

    Adding vocabularies in multiple languages.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""This is a sample Python script."""
 
 
 def main():
+    """Main function to execute the script."""
+    # Print a greeting message
     print("Hello from language-training!")
 
 
